641191b490f7152db023c537445ee0ccb6b88ffe
wolz-CODElife <joeladewole3@gmail.com>
2021-07-28 20:07:19 +0100

    Impleted contextApi

---
+++ src/Player.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Player.css'
+
+const Player = () => {
+    return (
+        <div>
+            <h2>Welcome to Spotify</h2>
+        </div>
+    )
+}
+
+export default Player
---
+++ src/reducer.js
@@ -0,0 +1,28 @@
+export const initialState = {
+    // change Token to null after developing
+    token: "BQCujwV0eMY_eiCgQmuVvGRTQyCeKQz6IXHope12w-A8KIYtvEYZp0CeeR_dcWYAjtBwHDQtDpzRz46GH49AU5Tltu9NObwN-L59yre1GUnDBfnaimVfhjeVzYsqaMrKyNvQwt8DQEdLevmRmiBIVVHrJ6EhWYG0GK18OuoSfB1uLAs03qxMXLW2",
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null
+}
+const reducer = (state, action) => {
+    console.log(action);
+    // action -> type, payload
+
+    switch (action.type) {
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.user
+            }
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
+            }
+        default:
+            return state
+    }
+}
+export default reducer
